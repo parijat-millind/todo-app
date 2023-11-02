1b64984834a001652605c0052c0ac7b82f427756
parijat millind <parijat.millind@gmail.com>
2023-11-02 09:29:29 +0530

    Update AuthComponent.js

--- frontend/src/components/todo/security/AuthComponent.js
+++ frontend/src/components/todo/security/AuthComponent.js
@@ -2,46 +2,40 @@ import React, { useContext, useState } from "react";
 import { authenticateUserApi } from "../api/TodoApiService";
 import { apiCLient } from "../api/apiClient";
 
-
 export const AuthContext = React.createContext();
 
 export const useAuth = () => useContext(AuthContext);
 
+
 export default function AuthProvider({ children }) {
 
-   
+
 
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [username, setUsername] = useState(null);
-    const [token, setToken] = useState(null);
-
 
 
     async function login(username, password) {
 
-        
-
-
         try {
             const response = await authenticateUserApi({ username, password })
-
-            if (response.data == "200") {
-                setIsAuthenticated(true);
-                setUsername(username);
+            if (response.status == "200") {
+                setIsAuthenticated(true)
+                setUsername(username)
                 const token = "Bearer " + response.data
                 apiCLient.interceptors.request.use((config) => {
                     config.headers.Authorization = token
                     return config
-                });
-                return true;
-            } else {
-                logout();
-                return false;
-            }
+                })
 
+                return true
+            } else {
+                logout()
+                return false
+            }
         } catch (error) {
-            logout();
-            return false;
+            logout()
+            return false
         }
 
     }
@@ -49,11 +43,10 @@ export default function AuthProvider({ children }) {
     function logout() {
         setIsAuthenticated(false);
         setUsername(null);
-        setToken(null);
     }
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, login, logout, username, token }}>
+        <AuthContext.Provider value={{ isAuthenticated, login, logout, username }}>
             {children}
         </AuthContext.Provider>
     )
